d6d16e8cd1dc83ba1c89dd9a5d13762f7a0358ea
Pablo Scolpino <martinibarra@gmail.com>
2017-10-21 02:14:33 +0000

    V6

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ var express = require("express")
   , bodyParser = require("body-parser")
   , app = express()
   , mongoose = require("mongoose")
+  , passport = require("passport")
+  , localStrategy = require("passport-local")
+  , User = require("./models/user")
   , Campground = require("./models/campground")
   , Comment = require("./models/comment")
   , seedDB = require("./seeds")
@@ -11,8 +14,33 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.set("view engine", "ejs")
 app.use(express.static(__dirname + "/public"))
 
-seedDB()
+//==========================================================================
+// PASSPORT CONFIGURATION
+//==========================================================================
 
+app.use(require("express-session")({
+  secret: "Once again Rusty wins cutest dog!",
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+passport.use(new localStrategy(User.authenticate()))
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
+
+app.use(function(req, res, next){
+  res.locals.currentUser = req.user
+  next()
+})
+//==========================================================================
+// ROUTES
+//==========================================================================
+// seedDB()
+
+//==========================================================================
+// ROUTES
+//==========================================================================
 app.get("/", function(req, res){
   res.render("landing")
 })
@@ -28,7 +56,7 @@ app.get("/campgrounds", function(req, res){
   })
 })
 
-app.post("/campgrounds", function(req, res){
+app.post("/campgrounds", isLoggedIn, function(req, res){
   var newCampground = {
     name: req.body.name,
     image: req.body.image,
@@ -45,7 +73,7 @@ app.post("/campgrounds", function(req, res){
   })
 })
 
-app.get("/campgrounds/new", function(req, res){
+app.get("/campgrounds/new", isLoggedIn, function(req, res){
   res.render("campgrounds/new")
 })
 
@@ -60,7 +88,7 @@ app.get("/campgrounds/:id", function(req, res) {
   })
 })
 
-app.get("/campgrounds/:id/comments/new", function (req, res) {
+app.get("/campgrounds/:id/comments/new", isLoggedIn, function (req, res) {
   Campground.findById(req.params.id, function(err, campground) {
     if (err) {
       console.log(err)
@@ -70,7 +98,7 @@ app.get("/campgrounds/:id/comments/new", function (req, res) {
   })
 })
 
-app.post("/campgrounds/:id/comments", function(req, res) {
+app.post("/campgrounds/:id/comments", isLoggedIn, function(req, res) {
   Campground.findById(req.params.id, function(err, campground) {
     if (err) {
       console.log(err)
@@ -88,6 +116,56 @@ app.post("/campgrounds/:id/comments", function(req, res) {
     }
   })
 })
+
+
+//==========================================================================
+// AUTH ROUTES
+//==========================================================================
+
+//show the register form
+app.get("/register", function(req, res){
+  res.render("register")
+})
+
+//handle sign up logic
+app.post("/register", function(req, res) {
+  newUser = new User({username: req.body.username})
+  User.register(newUser, req.body.password, function(err, user){
+    if (err) {
+      console.log(err)
+      res.render("register")
+    }
+    passport.authenticate("local")(req, res, function(){
+      res.redirect("/campgrounds")
+    })
+  })
+})
+
+app.get("/login", function(req, res){
+  res.render("login")
+})
+
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/campgrounds",
+    failureRedirect: "/login"
+  }), function(res, req){
+})
+
+app.get("/logout", function(req, res) {
+  req.logout()
+  res.redirect("/campgrounds")
+})
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next()
+  }
+  res.redirect("/login")
+}
+
+//==========================================================================
+// SERVER
+//==========================================================================
 
 app.listen(3000, function(){
   console.log("Server listening on port 3000");
